c1be7b41a2a02a2a5022464faeba8752d1f7ba61
CSTJ\1603235 <1603235@cstj.qc.ca>
2023-09-15 14:32:44 -0400

    Finalisation du style de la barre de navigation et ajout du fichier du component du footer

--- projet_web2/resources/views/components/nav.blade.php
+++ projet_web2/resources/views/components/nav.blade.php
@@ -9,16 +9,20 @@
                 <div class="close-icon" id="closeIcon">X</div>
                 <div class="liens">
                     <li><a href="{{ route('index') }}">Accueil</a></li>
-                    <li><a href="#">Billetterie</a></li>
-                    <li><a href="#">Activités</a></li>
-                    <li><a href="#">Actualités</a></li>
-                    <li><a href="#">Compte</a></li>
+                    <li><a href="{{ route('forfaits.show') }}">Billetterie</a></li>
+                    <li><a href="{{ route('activites.show') }}">Activités</a></li>
+                    <li><a href="{{ route('actualites.show') }}">Actualités</a></li>
+                    <li><a href="{{ route('user.reservation') }}">Mon compte</a></li>
+                    <li><a href="{{ route('user.create') }}">Créer un compte</a></li>
                 </div>
             </ul>
         </div>
         <a href="{{ route('login.show') }}">
             <img src="/img/icones/compte.png" class="image_compte" alt="Connexion">
         </a>
+        @if(auth()->check())
+            <a href="{{ route('logout') }}" class="logout">Déconnexion</a>
+        @endif
     </div>
     <a href="{{ route('index') }}">
         <img src="/img/logos/logo_blanc_transparent.png" class="logo" alt="Logo de MFMF Music Festival">
