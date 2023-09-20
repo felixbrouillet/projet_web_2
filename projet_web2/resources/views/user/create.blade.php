f8c8a21e617d3b0d0bf4b9d101930f5d9cfbb994
CSTJ\2138845 <jackcharbonneau@outlook.com>
2023-09-20 12:47:05 -0400

    Style sur la page de connexion et de création de compte

--- projet_web2/resources/views/user/create.blade.php
+++ projet_web2/resources/views/user/create.blade.php
@@ -1,38 +1,33 @@
-<form action="{{ route('user.store') }}" method="POST">
-    @csrf
-
-    <!-- Champ Prénom -->
-    <div class="form-cell">
-        <label for="prenom" class="form-label">Prénom</label>
-        <input type="text" class="form-control" id="prenom" name="prenom" value="{{ old('prenom') }}" required>
-    </div>
-
-    <!-- Champ Nom -->
-    <div class="form-cell">
-        <label for="nom" class="form-label">Nom</label>
-        <input type="text" class="form-control" id="nom" name="nom" value="{{ old('nom') }}" required>
-    </div>
-
-    <!-- Champ Email -->
-    <div class="form-cell">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
-    </div>
-
-    <!-- Champ Mot de passe -->
-    <div class="form-cell">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password" required>
-    </div>
-
-    <!-- Champ Confirmation du mot de passe -->
-    <div class="form-cell">
-        <label for="password_confirmation" class="form-label">Confirmation du mot de passe</label>
-        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-    </div>
-
-    <input type="hidden" name="role_id" value="2">
-
-    <!-- Bouton de soumission -->
-    <button type="submit">Créer un compte</button>
-</form>
+<body class="create">
+    <x-layout titre="Créer un compte">
+        <div class="container">
+            <h2>Créer un compte</h2>
+            <form class="formulaire create" action="{{ route('user.store') }}" method="POST">
+                @csrf
+                    <!-- Champ Prénom -->
+                    <div>
+                        <input type="text" id="prenom" name="prenom" value="{{ old('prenom') }}" placeholder="Prénom" required>
+                    </div>
+                    <!-- Champ Nom -->
+                    <div class="form-cell">
+                        <input type="text" id="nom" name="nom" value="{{ old('nom') }}" placeholder="Nom" required>
+                    </div>
+                    <!-- Champ Email -->
+                    <div>
+                        <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Email" required>
+                    </div>
+                    <!-- Champ Mot de passe -->
+                    <div>
+                        <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+                    </div>
+                    <!-- Champ Confirmation du mot de passe -->
+                    <div>
+                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmer le mot de passe" required>
+                    </div>
+                    <input type="hidden" name="role_id" value="2">
+                    <!-- Bouton de soumission -->
+                    <button type="submit">Créer un compte</button>
+            </form>
+        </div>
+    </x-layout>
+</body>
