c428119f3d5cbc44ff2d52659b983263eec5159f
CSTJ\1603235 <1603235@cstj.qc.ca>
2023-09-21 10:12:30 -0400

    Ajout du style sur la page de confirmation du forfait Ajout des balises script dans le layout pour GoogleAnalytics Ajout des balises meta pour un meilleur référencement prise 2

--- projet_web2/resources/views/actualites/index.blade.php
+++ projet_web2/resources/views/actualites/index.blade.php
@@ -1,8 +1,15 @@
 <x-layout titre="Actualités">
-    <h1>Nos actualités</h1>
-    @foreach($actualites as $actualite)
-        <p>{{ $actualite->nom }}</p>
-        <p>{{ $actualite->contenu }}</p>
-        <p>{{ $actualite->image }}</p>
-    @endforeach
+    <main class="actualites">
+        <h1>Actualités</h1>
+        @foreach ($actualites as $actualite)
+            <div class="actualite">
+                <h2>{{ $actualite->nom }}</h2>
+                <div class="contenu">
+                    <img src="{{ asset('img/images/' . $actualite->image) }}" alt="{{ $actualite->nom }}"
+                        class="actualite-image">
+                    <p>{{ $actualite->contenu }}</p>
+                </div>
+            </div>
+        @endforeach
+    </main>
 </x-layout>
