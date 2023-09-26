4f35c30087fb55298fa400f7803ae5c815ba63d1
CSTJ\1603235 <1603235@cstj.qc.ca>
2023-09-26 13:42:44 -0400

    Mise à jour de certaines pages qui étaient rendues des anciennes versions

--- projet_web2/resources/views/auth/login/register.blade.php
+++ projet_web2/resources/views/auth/login/register.blade.php
@@ -1,16 +1,17 @@
-<body class="auth">
+<div class="auth">
     <x-layout titre="Connexion">
         <div class="container">
 
-        {{-- Messages d'alertes succes ou erreur --}}
-        <x-alertes.alerte cle="succes" />
-        <x-alertes.alerte cle="erreur" />
+            {{-- Messages d'alertes succes ou erreur --}}
+            <x-alertes.alerte cle="succes" />
+            <x-alertes.alerte cle="erreur" />
 
-        <h2>Connexion</h2>
+            <h2>Connexion</h2>
             <form class="formulaire auth" action="{{ route('login.authentifier') }}" method="POST">
                 @csrf
                 <div>
-                    <input type="email" name="email" autocomplete="email" value="{{ old('email') }}" placeholder="Email">
+                    <input type="email" name="email" autocomplete="email" value="{{ old('email') }}"
+                        placeholder="Email">
                 </div>
                 <div>
                     <input type="password" name="password" autocomplete="current-password" placeholder="Mot de passe">
@@ -19,5 +20,6 @@
                 <a class="user-create" href="{{ route('user.create') }}">Pas de compte ?</a>
             </form>
         </div>
-    </x-layout>
-</body>
+</div>
+</x-layout>
+</div>
